646f7f957b0851abe17bd3c4d5f2b9cb562228ba
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-03-24 14:18:21 +0530

    app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import AppBar from "./componenets/AppBar"
 
 function App() {
+  const [user, setUser] = useState({})
+  useEffect(()=>{
+    async function call(){
+       const res = await axios.get('https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae')
+    setUser(res.data.user)        
+      }
+    call()
+  },[])
+
+  console.log(user)
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="w-screen h-[10000px] bg-gray-900/95 text-white">
+      <AppBar/>
     </div>
   )
 }
---
+++ src/componenets/AppBar.tsx
@@ -0,0 +1,17 @@
+function AppBar() {
+  return (
+    <div className=" flex  justify-center font-poppins text-sm md:text-lg lg:text-xl py-5 w-screen fixed ">
+      <div className="flex justify-center space-x-2 md:space-x-10 lg:space-x-16 bg-white/10 py-2 px-2 md:py-4 md:px-5 lg:py-7 lg:px-10 rounded-full w-max backdrop-blur-sm">
+        <div className="cursor-pointer">Home</div>
+        <div className="cursor-pointer">About</div>
+        <div className="cursor-pointer">Service</div>
+        <div className="cursor-pointer">Skills</div>
+        <div className="cursor-pointer">Projects</div>
+        <div className="cursor-pointer">Timeline</div>
+        <div className="cursor-pointer">Conatct Us</div>
+      </div>
+    </div>
+  );
+}
+
+export default AppBar;
